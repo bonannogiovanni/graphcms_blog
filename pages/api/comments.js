6d2e439b247ab4aa457952fbaec8039dcce09164
bonannogiovanni <bonannogiovanni@hotmail.com>
2022-01-17 22:51:26 +0100

    first commit

---
+++ pages/api/comments.js
@@ -0,0 +1,40 @@
+/**
+ * This is an API endpoint mapped to /api/*
+ */
+import { GraphQLClient, gql } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export default async function comments(req, res) {
+  const { name, email, slug, comment } = req.body;
+
+  const graphQLCLient = new GraphQLClient(graphqlAPI, {
+    headers: {
+      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+    },
+  });
+
+  const query = gql`
+    mutation CreateComment(
+      $name: String!
+      $email: String!
+      $comment: String!
+      $slug: String!
+    ) {
+      createComment(
+        data: {
+          name: $name
+          email: $email
+          comment: $comment
+          post: { connect: { slug: $slug } }
+        }
+      ) {
+        id
+      }
+    }
+  `;
+
+  const result = await graphQLCLient.request(query, req.body);
+
+  return res.status(200).send(result);
+}
