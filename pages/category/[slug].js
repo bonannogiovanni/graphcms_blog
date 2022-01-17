db79a1382df8199bb258c8d4c588ca7f7629fe02
bonannogiovanni <bonannogiovanni@hotmail.com>
2022-01-17 23:35:18 +0100

    second commit

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -32,6 +32,7 @@ export default CategoryPost;
 
 // Fetch data at build time
 export async function getStaticProps({ params }) {
+    
   const posts = await getCategoryPost(params.slug);
 
   return {
